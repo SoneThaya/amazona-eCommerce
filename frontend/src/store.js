f6ff769982f699fb8930d67473ebb814e691a332
Sone <sthaya@gmail.com>
2020-05-03 17:20:07 -0700

    worked on shopping card, can add quantity price will update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { productListReducer, productDetailsReducer } from './reducer/productReducers';
 import thunk from 'redux-thunk';
+import Cookie from 'js-cookie'
+import { cartReducer } from './reducer/cartReducers';
 
-const initialState = {};
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = {cart: {cartItems}};
+
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
