a61ab2bfb6699f3da326621590e3f4ba46b3ffab
Sone <sthaya@gmail.com>
2020-06-27 16:02:38 -0700

    create utils file, funtion to get token.

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export default {
-  MOGONDB_URL: process.env.MOGONDB_URL || 'mongodb://localhost/amazona'
+  MONGODB_URL: process.env.MOGONDB_URL || 'mongodb://localhost/amazona',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
